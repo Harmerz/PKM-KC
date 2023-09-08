b7e0b0adf9bbf099f4777546c038b3738e79b434
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-09-08 09:54:04 +0700

    feat : main py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('yolov8n-pose.pt')
+model = YOLO('yolov8n.pt')
 
-video_path = 0
+video_path = 'testJarak.mp4'
 cap = cv2.VideoCapture(video_path)
+cv2.namedWindow('frame')
 
+count = 1
 while cap.isOpened():
   success, frame = cap.read()
+  # sky = frame[350:450, 125:300]
+  sky = frame
   if success:
-    results = model(frame, save=True)
+    results = model(sky, save=True)
     annotated_frame = results[0].plot()
-    cv2.imshow("YOLOv8 Inference", annotated_frame)
+    print(annotated_frame)
+    cv2.imshow("frame", annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
       break
